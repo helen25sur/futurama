7436555c9f5f6e463f5fece6646d3dcc543f1e48
Olena Surilova <elen21surilova@gmail.com>
2026-02-09 22:10:49 +0100

    feat: create Logo component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import Navigation from '../Navigation/Navigation';
 import './Header.css';
+import Logo from '../Logo/Logo';
 
 export default function Header() {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
@@ -12,10 +13,7 @@ export default function Header() {
 
   return (
     <header className="header">
-      <div className="logo">
-        <img className="logo-icon" src="/images/icons/leading-icon.svg" alt="logo" />
-        <h2 className="header-title">Футурама&nbsp;фан</h2>
-      </div>
+      <Logo />
       <div className="video-icon">
         <button>
           <img src="/images/icons/trailing-icon.svg" alt="logo" />
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import './Logo.css';
+
+export default function Logo() {
+  return (
+    <div className="logo">
+      <a className="logo-link" href="/">
+        <img className="logo-icon" src="/images/icons/leading-icon.svg" alt="logo" /></a>
+      <h2 className="header-title">Футурама&nbsp;фан</h2>
+    </div>
+  )
+}
